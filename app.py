41b3716d8e8cc6cdaa1f604122bf11695815219a
Gabriel Coni F. Reis <gabrielreis.smtp@gmail.com>
2026-06-15 16:36:55 +0100

    Deploy ready

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -5,5 +6,8 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
